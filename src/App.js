4de9c87dbf7ee9bbe2cf1add1a2aa31c77bfe255
Kyprosa <propolis200@gmail.com>
2023-06-25 18:10:08 +0900

    16

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
 import React, { Component } from "react";
 import "./App.css";
-
+import TOC from "./components/TOC";
+import Subject from "./components/Subject";
+import Content from "./components/Content";
 class App extends Component {
   render() {
-    return <div class="app">hello! React!</div>;
+    return (
+      <div class="app">
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="For  UI"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperTextMarkupLanguage."></Content>
+      </div>
+    );
   }
 }
 
