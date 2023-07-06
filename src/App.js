26999547605a6def44862ded230c946d6838df17
Kyprosa <propolis200@gmail.com>
2023-07-06 17:59:49 +0900

    26

--- src/App.js
+++ src/App.js
@@ -58,7 +58,12 @@ class App extends Component {
           </h1>
           {this.state.subject.sub}
         </header> */}
-        <TOC data={this.state.contents}></TOC>
+        <TOC
+          onChangePage={function () {
+            this.setState({ mode: "read" });
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
